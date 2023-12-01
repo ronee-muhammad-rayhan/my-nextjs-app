fdb9333943543dc4310406651f0b8722f18114f9
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-12-01 18:06:16 +0600

    73-8 Explore Group Routing and explain built-in routes

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import Counter from "@/components/Counter/Counter";
 import React from "react";
 
 const HomePage = () => {
+  // console.log("Hello world!");
+  // throw new Error();
   return (
     <div>
       <h1>This is next app</h1>
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -6,7 +6,7 @@ const Counter = () => {
     return (
         <div>
             <h2>Counter: {counter}</h2>
-            <button className="btn btn-primary" onClick={() => setCounter(counter + 1)}>Increment</button>
+            <button className="btn btn-primary text-red-500" onClick={() => setCounter(counter + 1)}>Increment</button>
             <button className="btn btn-primary" onClick={() => setCounter(counter - 1)}>Decrement</button>
         </div>
     );
