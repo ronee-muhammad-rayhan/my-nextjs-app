ba4385432eaef754878dfadffd33da7ecb53398d
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-12-01 17:01:24 +0600

    73-7 Let’s dive deep into Server Component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Counter from "@/components/Counter/Counter";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div>
       <h1>This is next app</h1>
+      <Counter />
     </div>
   );
 };
---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,15 @@
+"use client";
+import { useState } from "react";
+
+const Counter = () => {
+    const [counter, setCounter] = useState(0)
+    return (
+        <div>
+            <h2>Counter: {counter}</h2>
+            <button className="btn btn-primary" onClick={() => setCounter(counter + 1)}>Increment</button>
+            <button className="btn btn-primary" onClick={() => setCounter(counter - 1)}>Decrement</button>
+        </div>
+    );
+};
+
+export default Counter;
